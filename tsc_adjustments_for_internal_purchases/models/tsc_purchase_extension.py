59b188fe2273745022929865c7e345d1f78ed340
szeben <juliusjaeger@gmail.com>
2023-09-11 19:24:26 -0400

    Update tsc_adjusment_internal_purchase

--- tsc_adjustments_for_internal_purchases/models/tsc_purchase_extension.py
+++ tsc_adjustments_for_internal_purchases/models/tsc_purchase_extension.py
@@ -20,11 +20,34 @@ class tsc_PurchaseOrder(models.Model):
             picking_type = picking_type.search([('tsc_picking_internal_use', '=', True)])
 
         if tsc_user_in_merchandise_group and not tsc_user_in_internal_group:
-            picking_type = picking_type.search([('tsc_picking_internal_use', '=', False)])
+            picking_type = picking_type.search([('tsc_picking_merchandise_use', '=', True)])
             
         return picking_type[:1]
 
-   
+    @api.model_create_multi
+    def create(self, vals_list):
+        self.tsc_check_values(vals_list[0])     
+        return super(tsc_PurchaseOrder, self).create(vals_list)
+
+    def write(self, vals):
+        self.tsc_check_values(vals)
+        return super(tsc_PurchaseOrder, self).write(vals)
+
+    def tsc_check_groups(self, tsc_picking_type_id, first_group, second_group, property):       
+        if first_group and not second_group:
+            if tsc_picking_type_id and not tsc_picking_type_id[property]:
+                raise UserError("No tiene permiso para operaciones de recepción de este tipo. Por favor compruebe su perfil como usuario de Compras.")
+
+    def tsc_check_values(self, vals):
+        tsc_user_in_internal_group = self.env.user.has_group('tsc_adjustments_for_internal_purchases.tsc_internal_purchases_group')
+        tsc_user_in_merchandise_group = self.env.user.has_group('tsc_adjustments_for_internal_purchases.tsc_merchandise_purchase_group')
+
+        if 'picking_type_id' in vals:
+            tsc_picking_type_id = self.env['stock.picking.type'].browse(vals.get('picking_type_id'))
+            self.tsc_check_groups(tsc_picking_type_id, tsc_user_in_internal_group, tsc_user_in_merchandise_group, "tsc_picking_internal_use")
+            self.tsc_check_groups(tsc_picking_type_id, tsc_user_in_merchandise_group, tsc_user_in_internal_group, "tsc_picking_merchandise_use")
+  
+                
 
     
     
--- tsc_adjustments_for_internal_purchases/models/tsc_stock_extension.py
+++ tsc_adjustments_for_internal_purchases/models/tsc_stock_extension.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models, tools, _, SUPERUSER_ID
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
-
-class tsc_StockLocation(models.Model):
-
-    _name = 'stock.location'
-    _inherit = ['stock.location', 'mail.thread']
-    
-    tsc_location_internal_use = fields.Boolean(string="Is it for Internal Purchases?", 
-                                              help="Mark as checked if the location will be used exclusively for internal purchase products.",
-                                              required=False,
-                                              readonly=False,
-                                              store=True,
-                                              copy=False,
-                                              tracking=False,
-                                              default=False)
-
 
 class tsc_StockPickingType(models.Model):
 
@@ -30,43 +15,14 @@ class tsc_StockPickingType(models.Model):
                                               copy=False,
                                               tracking=False,
                                               default=False)
-    @api.model
-    def create(self, vals):
-        if vals.get('tsc_picking_internal_use'):
-            tsc_src_location = self.env['stock.location'].browse(vals.get('default_location_src_id'))
-            tsc_dest_location = self.env['stock.location'].browse(vals.get('default_location_dest_id'))
-            if not tsc_src_location.tsc_location_internal_use and not tsc_dest_location.tsc_location_internal_use:
-                raise UserError('Para que la operación sea empleada para Compras Internas, la ubicación origen o la ubicación destino también debe ser para Compras Internas. Por favor, compruebe la configuración de las ubicaciones.')
-        
-        return super(tsc_StockPickingType, self).create(vals)
 
-    def write(self, vals):
-
-        for picking_type in self:
-           
-            if ('tsc_picking_internal_use' in vals and vals.get('tsc_picking_internal_use') == True)\
-            or picking_type.tsc_picking_internal_use:
-                
-                tsc_location_src_location = picking_type.default_location_src_id
-                tsc_location_dest_location = picking_type.default_location_dest_id
-
-                if vals.get('default_location_src_id'):
-                    tsc_id = vals.get('default_location_src_id')
-                    tsc_location_src_location = self.env['stock.location'].browse(tsc_id)
-
-                if vals.get('default_location_dest_id'):
-                    tsc_id = vals.get('default_location_dest_id')
-                    tsc_location_dest_location = self.env['stock.location'].browse(tsc_id)
-
-
-                if not tsc_location_src_location.tsc_location_internal_use and not tsc_location_dest_location.tsc_location_internal_use:
-                    raise UserError('Para que la operación sea empleada para Compras Internas, la ubicación origen o la ubicación destino también debe ser para Compras Internas. Por favor, compruebe la configuración de las ubicaciones.')
-            
-
-                    
-        
-        return super(tsc_StockPickingType, self).write(vals)
-
-
+    tsc_picking_merchandise_use = fields.Boolean(string="Is it for Merchandise Purchases?", 
+                                              help="Mark as checked if the operation will be used exclusively for merchandise purchases.",
+                                              required=False,
+                                              readonly=False,
+                                              store=True,
+                                              copy=False,
+                                              tracking=False,
+                                              default=False)
 
 
