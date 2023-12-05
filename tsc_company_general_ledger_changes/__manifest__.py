9c9658307ebea422c68eba0c8784e95deb39f142
Julio Jaeger <35298359+szeben@users.noreply.github.com>
2023-12-05 09:05:44 -0400

    Merge OXF040 manifest.py via upload

--- tsc_company_general_ledger_changes/__manifest__.py
+++ tsc_company_general_ledger_changes/__manifest__.py
@@ -1,37 +1,37 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Changes in general ledger by company",
+    'name': "Personalización al Reporte Vencidas por Cobrar",
 
     'summary': """
-        Includes an Estimated Receipt, Products Received, and Products in Transit date column for supplier invoice records in a company's General Ledger. Make a distinction by color according to the state of receipt of the merchandise""",
+        Nueva Columna (DÍAS DE EMISIÓN) y se modificó el Rango 
+    """,
 
     'description': """
-        Includes an Estimated Receipt, Products Received, and Products in Transit date column for supplier invoice records in a company's General Ledger. Make a distinction by color according to the state of receipt of the merchandise. Includes a user group that can specify whether the products of a purchase order are in transit
+       Al lado derecho de la columna CUENTA, debe aparecer una columna (DÍAS DE
+       EMISIÓN) que refleje los días transcurridos desde la emisión de la factura.
+       - En los rangos presentes en el reporte, se deben incluir los rangos de 1-15
     """,
 
     'author': "Techne Studio IT & Consulting",
     'website': "https://technestudioit.com/",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '15.0.1.1',
-    'license': "Other proprietary",
+    'version': '0.1',
+    'license': "Other proprietary",	
 
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'tsc_mark_products_received', 'branch_accounting_report'],
+    # any module necessary for this one to work correctly ..
+    'depends': ['base', 'account_reports', 'branch_accounting_report'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'security/tsc_security.xml',
-        'views/tsc_purchase_view.xml'
     ],
+
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
 }
