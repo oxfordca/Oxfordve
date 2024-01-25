58369cc5381dc940425ffc8ed07d09550cbc7749
Julio Jaeger <35298359+szeben@users.noreply.github.com>
2024-01-25 09:34:59 -0400

    Credit Note Confirmation

--- tsc_restrict_credit_note_confirmation/models/models.py
+++ tsc_restrict_credit_note_confirmation/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import _, api, exceptions, fields, models
+from odoo import _, api, exceptions, fields, models, _
 from odoo.exceptions import UserError
 
 class TSCAccountMove(models.Model):
@@ -11,6 +11,19 @@ class TSCAccountMove(models.Model):
         compute="_compute_tsc_check_user_group",
         store=False
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+
+        res = super(TSCAccountMove, self).create(vals_list)
+        
+        tsc_has_group = self.env.user.has_group(
+            'tsc_restrict_credit_note_confirmation.tsc_group_create_customer_corrective_invoices'
+        )
+        if res.move_type == 'out_refund' and not tsc_has_group:
+            raise UserError(_('Due to security restrictions, you have no permission to validate this operation.'))        
+            
+        return res
 
     @api.depends('state')
     def _compute_tsc_check_user_group(self):
