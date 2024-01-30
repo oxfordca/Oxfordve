f633173e9bff122e57d780f4f6fd372e0192ece9
Julio Jaeger <35298359+szeben@users.noreply.github.com>
2024-01-30 10:41:10 -0400

    Add files via upload

--- restrictions_for_bank_statements_and_conciliation/__manifest__.py
+++ restrictions_for_bank_statements_and_conciliation/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting',
-    'version': '3.1.1',
+    'version': '3.1',
 
     'license': 'Other proprietary',
 
