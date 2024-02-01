614f0f8c8b1a23365a8ed6d6663777fe946aa90d
Julio Jaeger <35298359+szeben@users.noreply.github.com>
2024-01-31 20:13:26 -0400

    Fix Credit Note

--- tsc_restrict_credit_note_confirmation/models/models.py
+++ tsc_restrict_credit_note_confirmation/models/models.py
@@ -14,14 +14,13 @@ class TSCAccountMove(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-
-        res = super(TSCAccountMove, self).create(vals_list)
-        
+        res = super(TSCAccountMove, self).create(vals_list)        
         tsc_has_group = self.env.user.has_group(
             'tsc_restrict_credit_note_confirmation.tsc_group_create_customer_corrective_invoices'
         )
-        if res.move_type == 'out_refund' and not tsc_has_group:
-            raise UserError(_('Due to security restrictions, you have no permission to validate this operation.'))        
+        for record in res:
+            if record.move_type == 'out_refund' and not tsc_has_group:
+                raise UserError(_('Due to security restrictions, you have no permission to validate this operation.'))        
             
         return res
 
