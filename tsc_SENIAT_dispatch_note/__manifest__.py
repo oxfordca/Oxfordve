dd828bbeb31ff6a431a8082c948a00e028f8452c
Julio Jaeger <35298359+szeben@users.noreply.github.com>
2024-07-02 08:49:49 -0400

    Commit Via Upload tsc_seniat_dispatch_mode

--- tsc_SENIAT_dispatch_note/__manifest__.py
+++ tsc_SENIAT_dispatch_note/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory/Inventory',
-    'version': '15.0.1.4',
+    'version': '15.0.2.0',
     'license': 'Other proprietary',
     # any module necessary for this one to work correctly
     'depends': ['base', 'branch', 'sequences_for_OS_and_guides'],
