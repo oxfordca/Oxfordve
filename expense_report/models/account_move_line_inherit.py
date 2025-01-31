589d0e61c0c2ea01f903e5031d6095e033f27bd2
AdrianJAS <adrianalvessaenz@gmail.com>
2025-01-31 09:55:06 -0400

    chore(expense_report): added more fields to tree view of report

--- expense_report/models/account_move_line_inherit.py
+++ expense_report/models/account_move_line_inherit.py
@@ -6,6 +6,8 @@ class AccountMoveLineInherit(models.Model):
 
     week = fields.Integer(string="Week", compute="_compute_week")
     type_of_account = fields.Many2one( string="Type of Account", related="account_id.user_type_id")
+    usd_amount = fields.Float(string="USD Amount", compute="_compute_expense_report_amount")
+    bs_amount = fields.Float(string="Bs Amount", compute="_compute_expense_report_amount")
 
     @api.depends('date')
     def _compute_week(self):
@@ -16,3 +18,12 @@ class AccountMoveLineInherit(models.Model):
                 record.week = 0
 
 
+    @api.depends('amount_currency', 'currency_id', 'balance')
+    def _compute_expense_report_amount(self):
+        for record in self:
+            if record.currency_id != record.company_currency_id:
+                record.usd_amount = record.balance
+                record.bs_amount = record.amount_currency
+            else:
+                record.usd_amount = record.amount_currency
+                record.bs_amount = 0
